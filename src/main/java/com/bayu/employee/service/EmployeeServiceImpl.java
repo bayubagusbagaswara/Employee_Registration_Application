d0f7a0d68fdd277c128542ff1173a083bec84988
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 10:30:05 +0700

    add service impl EmployeeServiceImpl

---
+++ src/main/java/com/bayu/employee/service/EmployeeServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bayu.employee.service;
+
+import com.bayu.employee.model.Employee;
+import com.bayu.employee.repository.EmployeeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
