9513031516d3187d4d0c83c24e97fcda9873901b
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-28 10:29:44 +0700

    add service EmployeeService

---
+++ src/main/java/com/bayu/employee/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package com.bayu.employee.service;
+
+import com.bayu.employee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> getAllEmployees();
+}
