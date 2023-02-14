a09db65eb4518808c1b187c62607e2d66540413c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-14 13:43:17 +0700

    add AuthController

---
+++ src/main/java/com/bayu/employee/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.bayu.employee.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthController {
+
+
+    // view form register
+
+    // post data register
+
+    // view form login
+
+    // post data login
+
+}
