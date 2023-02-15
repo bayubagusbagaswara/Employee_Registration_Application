7375641f37f8ce07d6fcf8facd12c63cc611f03f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 10:43:30 +0700

    update HomeController

--- src/main/java/com/bayu/employee/controller/HomeController.java
+++ src/main/java/com/bayu/employee/controller/HomeController.java
@@ -1,9 +1,16 @@
 package com.bayu.employee.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     // handler for Home Page
+
+    // handler for Home Page, jika User berhasil login
+    @GetMapping("/home/user")
+    public String homeUser() {
+        return "employee/index";
+    }
 }
