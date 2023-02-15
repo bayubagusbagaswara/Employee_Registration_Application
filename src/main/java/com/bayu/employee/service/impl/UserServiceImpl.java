2c004a70fb2066bb5b856cc74725d52f19570618
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 18:37:23 +0700

    update service UserServiceImpl

--- src/main/java/com/bayu/employee/service/impl/UserServiceImpl.java
+++ src/main/java/com/bayu/employee/service/impl/UserServiceImpl.java
@@ -1,38 +1,54 @@
 package com.bayu.employee.service.impl;
 
 import com.bayu.employee.model.User;
-import com.bayu.employee.payload.LoginRequest;
-import com.bayu.employee.payload.LoginResponse;
-import com.bayu.employee.payload.RegistrationRequest;
-import com.bayu.employee.payload.RegistrationResponse;
+import com.bayu.employee.payload.*;
 import com.bayu.employee.repository.UserRepository;
 import com.bayu.employee.service.UserService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
-    public RegistrationResponse register(RegistrationRequest registrationRequest) {
+    public Boolean checkUsernameIsExists(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public Boolean checkEmailIsExists(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public void register(RegistrationRequest registrationRequest) {
         // create User
         User user = User.builder()
                 .username(registrationRequest.getUsername())
                 .email(registrationRequest.getEmail())
-                .password(registrationRequest.getPassword())
+                .password(passwordEncoder.encode(registrationRequest.getPassword()))
+                .enabled(true)
                 .build();
 
         userRepository.save(user);
-        return null;
     }
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
+        return null;
+    }
+
+    @Override
+    public UserDTO getUserByEmail(String email) {
         return null;
     }
 
