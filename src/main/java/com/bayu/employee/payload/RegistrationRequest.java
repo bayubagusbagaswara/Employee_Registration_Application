51856c35e1195c302d5095329547fac7bc63b893
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 15:04:05 +0700

    add validation

--- src/main/java/com/bayu/employee/payload/RegistrationRequest.java
+++ src/main/java/com/bayu/employee/payload/RegistrationRequest.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
@@ -11,11 +15,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegistrationRequest {
 
+    @NotBlank(message = "Username must not be blank")
     private String username;
 
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Enter a valid email address")
     private String email;
 
+    @NotBlank(message = "Password must not be blank")
+    @Length(min = 6, message = "Password must be at least 6 characters")
     private String password;
 
+    @NotBlank(message = "Repeat Password must not be blank")
     private String repeatPassword;
 }
