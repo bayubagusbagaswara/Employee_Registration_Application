92dbc96eb9fab899f4947177613cde49941ed37b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 14:49:32 +0700

    update HomeController

--- src/main/java/com/bayu/employee/controller/HomeController.java
+++ src/main/java/com/bayu/employee/controller/HomeController.java
@@ -9,6 +9,7 @@ public class HomeController {
     // handler for Home Page
 
     // handler for Home Page, jika User berhasil login
+    // ini untuk menggantikan URL default nya "/"
     @GetMapping("/home/user")
     public String homeUser() {
         return "employee/index";
