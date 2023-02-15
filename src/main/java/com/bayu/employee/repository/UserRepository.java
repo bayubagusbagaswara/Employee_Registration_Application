d3094cd572da134e0f9bb1ad7a20bcb98d6d5d4c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 15:45:28 +0700

    add methods

--- src/main/java/com/bayu/employee/repository/UserRepository.java
+++ src/main/java/com/bayu/employee/repository/UserRepository.java
@@ -2,6 +2,26 @@ package com.bayu.employee.repository;
 
 import com.bayu.employee.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    List<User> findByUsernameContainingIgnoreCase(String username);
+
+    @Query("SELECT u FROM User u WHERE u.username LIKE %:username%")
+    List<User> searchByUsernameLike(@Param("username") String username);
 }
