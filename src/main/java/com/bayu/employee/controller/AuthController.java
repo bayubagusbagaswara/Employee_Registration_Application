8028acddc3efc758bfeaf10a59ce473e204154a6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 09:13:33 +0700

    update AuthController

--- src/main/java/com/bayu/employee/controller/AuthController.java
+++ src/main/java/com/bayu/employee/controller/AuthController.java
@@ -1,12 +1,17 @@
 package com.bayu.employee.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthController {
 
 
     // view form register
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
 
     // post data register
 
