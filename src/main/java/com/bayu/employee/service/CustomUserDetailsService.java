11cdd56b5fc46c71fdf27eef67cb76387a1b47ce
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 18:35:50 +0700

    add service CustomUserDetailsService

---
+++ src/main/java/com/bayu/employee/service/CustomUserDetailsService.java
@@ -0,0 +1,15 @@
+package com.bayu.employee.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface CustomUserDetailsService extends UserDetailsService {
+
+    @Override
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+
+    // loadUserByUsernameOrEmail(String username, String email)
+
+    // loadUserById(String id)
+}
