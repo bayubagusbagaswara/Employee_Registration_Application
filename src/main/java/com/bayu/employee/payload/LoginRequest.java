f3db76372520571f59d45fc9af661df75c46b9a0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-15 18:36:29 +0700

    update dto LoginRequest

--- src/main/java/com/bayu/employee/payload/LoginRequest.java
+++ src/main/java/com/bayu/employee/payload/LoginRequest.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginRequest {
 
-    private String usernameOrEmail;
+    private String username;
 
     private String password;
 }
