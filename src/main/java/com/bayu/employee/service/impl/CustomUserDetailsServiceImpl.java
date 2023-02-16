41743089b2711092aa2959f2d19e7a2f082db94e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-16 11:38:26 +0700

    update service CustomUserDetailsServiceImpl

--- src/main/java/com/bayu/employee/service/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/bayu/employee/service/impl/CustomUserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package com.bayu.employee.service.impl;
 
+import com.bayu.employee.model.User;
 import com.bayu.employee.repository.UserRepository;
+import com.bayu.employee.security.CustomUserDetails;
 import com.bayu.employee.service.CustomUserDetailsService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,6 +21,10 @@ public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         // disini kita nanti juga ambil role tiap user nya, lalu cek role nya apa
-        return null;
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User not found with this username : %s", username)));
+
+        return CustomUserDetails.mapFromUser(user);
     }
+
 }
