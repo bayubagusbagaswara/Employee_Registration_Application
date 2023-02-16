a5e744a9577e03b6e0677ea9bf5fe525aa0c683e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-16 16:50:45 +0700

    update HomeController

--- src/main/java/com/bayu/employee/controller/HomeController.java
+++ src/main/java/com/bayu/employee/controller/HomeController.java
@@ -1,17 +1,16 @@
 package com.bayu.employee.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    // handler for Home Page
-
-    // handler for Home Page, jika User berhasil login
-    // ini untuk menggantikan URL default nya "/"
-    @GetMapping("/home/user")
-    public String homeUser() {
-        return "employee/index";
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home - Employee Registration");
+        return "home";
     }
+
 }
