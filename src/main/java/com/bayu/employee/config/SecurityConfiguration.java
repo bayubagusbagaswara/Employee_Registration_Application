c73d48049d4e2385ca2081726db01b98e22086a9
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-17 10:03:24 +0700

    update SecurityConfiguration

--- src/main/java/com/bayu/employee/config/SecurityConfiguration.java
+++ src/main/java/com/bayu/employee/config/SecurityConfiguration.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,7 +25,6 @@ public class SecurityConfiguration {
             "/webjars/**"
     };
 
-    // kita buat Success Handler  untuk menghandle setelah login berhasil
     private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
 
     public SecurityConfiguration(CustomAuthenticationSuccessHandler authenticationSuccessHandler) {
