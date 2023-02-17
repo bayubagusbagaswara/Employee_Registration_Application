f1c4c0efae0ea144427abc18433f4123d0b31d56
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-17 10:03:38 +0700

    update UserController

--- src/main/java/com/bayu/employee/controller/UserController.java
+++ src/main/java/com/bayu/employee/controller/UserController.java
@@ -2,12 +2,15 @@ package com.bayu.employee.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/index")
 public class UserController {
 
-    @GetMapping("/index")
+    @GetMapping
     public String index() {
         return "user/index.html";
     }
+
 }
