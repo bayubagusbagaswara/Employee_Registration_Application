72b5db0d5eb43167d69126e8ca7dbf08b927452f
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-17 08:55:36 +0700

    update testing UserServiceImplTest

--- src/test/java/com/bayu/employee/service/impl/UserServiceImplTest.java
+++ src/test/java/com/bayu/employee/service/impl/UserServiceImplTest.java
@@ -38,9 +38,9 @@ class UserServiceImplTest {
     @Test
     void createUser() {
         CreateUserRequest createUserRequest = CreateUserRequest.builder()
-                .username("bayu")
-                .email("bayu@mail.com")
-                .password("bayu123")
+                .username("albert")
+                .email("albert@mail.com")
+                .password("albert123")
                 .build();
 
         UserDTO user = userService.createUser(createUserRequest);
@@ -57,12 +57,31 @@ class UserServiceImplTest {
     @Test
     void createAdmin() {
         CreateUserRequest createUserRequest = CreateUserRequest.builder()
-                .username("albert")
-                .email("albert@mail.com")
-                .password("albert123")
+                .username("admin")
+                .email("admin@mail.com")
+                .password("admin123")
                 .build();
 
         UserDTO user = userService.createAdmin(createUserRequest);
+
+        assertNotNull(user.getId());
+
+        log.info("ID: {}", user.getId());
+        log.info("Username: {}", user.getUsername());
+        log.info("Email: {}", user.getEmail());
+        log.info("Password: {}", user.getPassword());
+        log.info("Role: {}", user.getRoles().toString());
+    }
+
+    @Test
+    void createAdminAndUser() {
+        CreateUserRequest createUserRequest = CreateUserRequest.builder()
+                .username("bayu")
+                .email("bayu@mail.com")
+                .password("bayu123")
+                .build();
+
+        UserDTO user = userService.createAdminAndUser(createUserRequest);
 
         assertNotNull(user.getId());
 
