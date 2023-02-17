11d29e297ad60cce5d17c6415bf9b8cfbf7c3bd2
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-17 10:03:06 +0700

    update AdminController

--- src/main/java/com/bayu/employee/controller/AdminController.java
+++ src/main/java/com/bayu/employee/controller/AdminController.java
@@ -1,25 +1,15 @@
 package com.bayu.employee.controller;
 
-import com.bayu.employee.service.EmployeeService;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
-    private final EmployeeService employeeService;
-
-    public AdminController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
-    @GetMapping("/admin")
+    @GetMapping
     public String homeAdmin() {
-        // mengarah ke admin.html
-        // misal kita coba tidak masuk kedalam folder admin, karena kita simpan admin.html didalam folder admin
         return "admin/admin.html";
     }
 
