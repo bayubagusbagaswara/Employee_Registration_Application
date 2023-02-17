06a67752a7eeeb11c43ea5343cfe43369afcda17
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-17 10:03:14 +0700

    update CustomAuthenticationSuccessHandler

--- src/main/java/com/bayu/employee/security/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/bayu/employee/security/CustomAuthenticationSuccessHandler.java
@@ -29,64 +29,64 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 
     private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
-//    @Override
-//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-//
-//        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
-//
-//        if (roles.contains(RoleName.ADMIN.getRoleName())) {
-//            response.sendRedirect("/admin");
-//        } else {
-//            response.sendRedirect("/index");
-//        }
-//    }
-
     @Override
-    public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException {
-        handle(request, response, authentication);
-        clearAuthenticationAttributes(request);
-    }
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-    protected void handle(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException {
-        final String targetUrl = determineTargetUrl(authentication);
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
-        if (response.isCommitted()) {
-            log.debug("Response has already been committed. Unable to redirect to " + targetUrl);
-            return;
+        if (roles.contains(RoleName.ADMIN.getRoleName())) {
+            response.sendRedirect("/admin");
+        } else {
+            response.sendRedirect("/index");
         }
-
-        redirectStrategy.sendRedirect(request, response, targetUrl);
     }
 
-    protected String determineTargetUrl(final Authentication authentication) {
-
-        Map<String, String> roleTargetUrlMap = new HashMap<>();
-        roleTargetUrlMap.put("USER", "/index");
-        roleTargetUrlMap.put("ADMIN", "/admin");
-
-        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        for (final GrantedAuthority grantedAuthority : authorities) {
-            String authorityName = grantedAuthority.getAuthority();
-            if(roleTargetUrlMap.containsKey(authorityName)) {
-                return roleTargetUrlMap.get(authorityName);
-            }
-        }
-
-        throw new IllegalStateException();
-    }
-
-    /**
-     * Removes temporary authentication-related data which may have been stored in the session
-     * during the authentication process.
-     */
-    protected final void clearAuthenticationAttributes(final HttpServletRequest request) {
-        final HttpSession session = request.getSession(false);
-
-        if (session == null) {
-            return;
-        }
-
-        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
-    }
+//    @Override
+//    public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException {
+//        handle(request, response, authentication);
+//        clearAuthenticationAttributes(request);
+//    }
+//
+//    protected void handle(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException {
+//        final String targetUrl = determineTargetUrl(authentication);
+//
+//        if (response.isCommitted()) {
+//            log.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+//            return;
+//        }
+//
+//        redirectStrategy.sendRedirect(request, response, targetUrl);
+//    }
+//
+//    protected String determineTargetUrl(final Authentication authentication) {
+//
+//        Map<String, String> roleTargetUrlMap = new HashMap<>();
+//        roleTargetUrlMap.put("USER", "/index");
+//        roleTargetUrlMap.put("ADMIN", "/admin");
+//
+//        final Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+//        for (final GrantedAuthority grantedAuthority : authorities) {
+//            String authorityName = grantedAuthority.getAuthority();
+//            if(roleTargetUrlMap.containsKey(authorityName)) {
+//                return roleTargetUrlMap.get(authorityName);
+//            }
+//        }
+//
+//        throw new IllegalStateException();
+//    }
+//
+//    /**
+//     * Removes temporary authentication-related data which may have been stored in the session
+//     * during the authentication process.
+//     */
+//    protected final void clearAuthenticationAttributes(final HttpServletRequest request) {
+//        final HttpSession session = request.getSession(false);
+//
+//        if (session == null) {
+//            return;
+//        }
+//
+//        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+//    }
 
 }
