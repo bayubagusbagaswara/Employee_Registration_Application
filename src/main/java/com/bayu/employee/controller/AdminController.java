dde9d73428f761d663ba812d86a4de59f91dff86
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-17 08:20:37 +0700

    update AdminController

--- src/main/java/com/bayu/employee/controller/AdminController.java
+++ src/main/java/com/bayu/employee/controller/AdminController.java
@@ -1,13 +1,13 @@
 package com.bayu.employee.controller;
 
 import com.bayu.employee.service.EmployeeService;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
 public class AdminController {
 
     private final EmployeeService employeeService;
@@ -16,12 +16,11 @@ public class AdminController {
         this.employeeService = employeeService;
     }
 
-    // Get All Employee atau bisa cari cara menampilkan list, biasanya sesudah menambahkan data baru, maka akan ditampilkan dalam list
-    @GetMapping("/employees")
-    public String getAllEmployees(Model model) {
-
-        // looping satu-satu datanya, lalu masukkan kedalam table
-
-        return "";
+    @GetMapping("/admin")
+    public String homeAdmin() {
+        // mengarah ke admin.html
+        // misal kita coba tidak masuk kedalam folder admin, karena kita simpan admin.html didalam folder admin
+        return "admin/admin.html";
     }
+
 }
