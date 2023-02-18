30befffd0d20e69973916723b66aa57a5aeaccf5
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-18 10:47:54 +0700

    implement all method service

--- src/main/java/com/bayu/employee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/bayu/employee/service/impl/EmployeeServiceImpl.java
@@ -1,58 +1,124 @@
 package com.bayu.employee.service.impl;
 
+import com.bayu.employee.exception.ResourceNotFoundException;
 import com.bayu.employee.model.Employee;
+import com.bayu.employee.payload.employee.CreateEmployeeRequest;
+import com.bayu.employee.payload.employee.EmployeeDTO;
+import com.bayu.employee.payload.employee.UpdateEmployeeRequest;
 import com.bayu.employee.repository.EmployeeRepository;
 import com.bayu.employee.service.EmployeeService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
-    @Override
-    public List<Employee> getAllEmployees() {
-        return employeeRepository.findAll();
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
-    public void saveEmployee(Employee employee) {
+    public List<EmployeeDTO> getAllEmployees() {
+        return employeeRepository.findAll()
+                .stream()
+                .map(employee -> EmployeeDTO.builder()
+                        .id(employee.getId())
+                        .position(employee.getPosition())
+                        .name(employee.getName())
+                        .age(String.valueOf(employee.getAge()))
+                        .address(employee.getAddress())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeById(String id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id : " + id));
+        return EmployeeDTO.builder()
+                .id(employee.getId())
+                .position(employee.getPosition())
+                .name(employee.getName())
+                .age(String.valueOf(employee.getAge()))
+                .address(employee.getAddress())
+                .build();
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeByName(String name) {
+        Employee employee = employeeRepository.findByNameContainsIgnoreCase(name)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with name : " + name));
+        return EmployeeDTO.builder()
+                .id(employee.getId())
+                .position(employee.getPosition())
+                .name(employee.getName())
+                .age(String.valueOf(employee.getAge()))
+                .address(employee.getAddress())
+                .build();
+    }
+
+    @Override
+    public EmployeeDTO createEmployee(CreateEmployeeRequest createEmployeeRequest) {
+        Employee employee = Employee.builder()
+                .name(createEmployeeRequest.getName())
+                .position(createEmployeeRequest.getPosition())
+                .age(Integer.valueOf(createEmployeeRequest.getAge()))
+                .address(createEmployeeRequest.getAddress())
+                .build();
+
         employeeRepository.save(employee);
+
+        return EmployeeDTO.builder()
+                .id(employee.getId())
+                .position(employee.getPosition())
+                .name(employee.getName())
+                .age(String.valueOf(employee.getAge()))
+                .address(employee.getAddress())
+                .build();
     }
 
     @Override
-    public Employee getEmployeeById(String id) {
-        Optional<Employee> optionalEmployee = employeeRepository.findById(id);
-        Employee employee = null;
+    public EmployeeDTO updateEmployee(String id, UpdateEmployeeRequest updateEmployeeRequest) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id : " + id));
 
-        if (optionalEmployee.isPresent()) {
-            employee = optionalEmployee.get();
-        } else {
-            throw new RuntimeException("Employee not found for id : " + id);
+        if (updateEmployeeRequest.getName() != null) {
+            employee.setName(updateEmployeeRequest.getName());
         }
-        return employee;
+
+        if (updateEmployeeRequest.getPosition() != null) {
+            employee.setPosition(updateEmployeeRequest.getPosition());
+        }
+
+        if (updateEmployeeRequest.getAge() != null) {
+            employee.setAge(Integer.valueOf(updateEmployeeRequest.getAge()));
+        }
+
+        if (updateEmployeeRequest.getAddress() != null) {
+            employee.setAddress(updateEmployeeRequest.getAddress());
+        }
+
+        employeeRepository.save(employee);
+
+        return EmployeeDTO.builder()
+                .id(employee.getId())
+                .position(employee.getPosition())
+                .name(employee.getName())
+                .age(String.valueOf(employee.getAge()))
+                .address(employee.getAddress())
+                .build();
     }
 
     @Override
-    public void deleteEmployeeById(String id) {
-        employeeRepository.deleteById(id);
+    public void deleteEmployee(String id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id : " + id));
+
+        employeeRepository.delete(employee);
     }
 
-    @Override
-    public Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection) {
-        Sort sort = sortDirection.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
-                Sort.by(sortField).descending();
-
-        Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sort);
-        return employeeRepository.findAll(pageable);
-    }
 }
