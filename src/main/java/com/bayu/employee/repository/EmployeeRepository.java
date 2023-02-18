9a0dc4b00f507dbe3fd84577150c02fe7f1be6a1
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-18 10:47:21 +0700

    add method findByName

--- src/main/java/com/bayu/employee/repository/EmployeeRepository.java
+++ src/main/java/com/bayu/employee/repository/EmployeeRepository.java
@@ -4,7 +4,11 @@ import com.bayu.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, String> {
+
+    Optional<Employee> findByNameContainsIgnoreCase(String name);
 
 }
