02b875cd0216fc110a9691521f07d491ac9f81cb
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-18 11:15:41 +0700

    update model Role

--- src/main/java/com/bayu/employee/model/Role.java
+++ src/main/java/com/bayu/employee/model/Role.java
@@ -2,6 +2,7 @@ package com.bayu.employee.model;
 
 import com.bayu.employee.model.enumerator.RoleName;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -15,8 +16,9 @@ import javax.persistence.*;
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    @GeneratedValue(generator = "system-uuid")
+    private String id;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "name", length = 20, nullable = false)
