21d8e2f01422c002691061d893f91e63cbc17423
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-18 17:19:52 +0700

    update payload CreateEmployeeRequest

--- src/main/java/com/bayu/employee/payload/employee/CreateEmployeeRequest.java
+++ src/main/java/com/bayu/employee/payload/employee/CreateEmployeeRequest.java
@@ -5,16 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateEmployeeRequest {
 
+    @NotBlank(message = "Position must not be blank")
     private String position;
+
+    @NotBlank(message = "Name must not be blank")
     private String name;
 
+    @NotBlank(message = "Age must not be blank")
     private String age;
 
+    @NotBlank(message = "Address must not be blank")
     private String address;
 }
