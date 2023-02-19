d343da3bf5907df85a1afd04c768d664bb36b1d4
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-19 07:53:28 +0700

    update all method

--- src/main/java/com/bayu/employee/controller/EmployeeController.java
+++ src/main/java/com/bayu/employee/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.bayu.employee.controller;
 
+import com.bayu.employee.model.Employee;
+import com.bayu.employee.model.User;
 import com.bayu.employee.payload.employee.CreateEmployeeRequest;
 import com.bayu.employee.payload.employee.EmployeeDTO;
 import com.bayu.employee.service.EmployeeService;
 import com.bayu.employee.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,14 +16,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.security.Principal;
 import java.util.List;
 
-/**
- * Yang bisa dilakukan oleh endpoint ini adalah selain get employee
- */
 @Controller
-@RequestMapping("/employees")
 public class EmployeeController {
 
     private final static Logger log = LoggerFactory.getLogger(EmployeeController.class);
@@ -33,35 +31,48 @@ public class EmployeeController {
         this.userService = userService;
     }
 
-    @GetMapping
-    public String myProfile(Model model, Principal principal) {
-        // cek apakah user sudah memiliki employee atau belum
+    @GetMapping("/employees")
+    public String personalData(Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
 
-        String name = principal.getName();
-        // cari user berdasarkan name
+        String name = authentication.getName(); // username
 
-        // lalu cari employee berdasarkan id user
+        log.info("Name: {}", name);
 
-        // jika user belum memiliki data employee (employee == null), maka redirect ke "redirect:/employee/show-form-employee"
-        // jika user sudah memiliki data employee, maka redirect ke "redirect:/employees/{employeeId}"
-
+        // cari user berdasarkan username
+        User user = userService.findByUsername(name);
 
         CreateEmployeeRequest createEmployeeRequest = new CreateEmployeeRequest();
+
+        // cari employee berdasarkan user_id
+        if (user.getEmployee() == null) {
+            model.addAttribute("createEmployeeRequest", createEmployeeRequest);
+            return "redirect:/employees/show-form-employee";
+        }
+
+        Employee employee = employeeService.findByUserId(user.getId());
+
+        model.addAttribute("employeeId", employee.getId());
+        return "redirect:/employees/{employeeId}";
+    }
+
+    @GetMapping("/employees/show-form-employee")
+    public String showNewEmployeeForm(Model model, Authentication authentication, RedirectAttributes redirectAttributes) {
+
+        // cari user by username
+        String name = authentication.getName();
+        User user = userService.findByUsername(name);
+
+        // redirect user id ke form
+        CreateEmployeeRequest createEmployeeRequest = new CreateEmployeeRequest();
         model.addAttribute("createEmployeeRequest", createEmployeeRequest);
+        model.addAttribute("userId", user.getId());
+
         return "employee/new_employee";
     }
 
-    @GetMapping("/show-form-employee")
-    public String showNewEmployeeForm(Model model) {
-        // saat kita get atau mengambil form ini, maka kita buatkan model kosong
-        // fungsinya model ini akan dimapping kedalam form nya
-        CreateEmployeeRequest createEmployeeRequest = new CreateEmployeeRequest();
-        model.addAttribute("createEmployeeRequest", createEmployeeRequest);
-        return "employee/new_employee";
-    }
-
-    @PostMapping("/saveEmployee")
+    @PostMapping("/employees/saveEmployee/{userId}")
     public String saveEmployee(@Valid @ModelAttribute("createEmployeeRequest") CreateEmployeeRequest createEmployeeRequest,
+                                 @PathVariable(value = "userId") String userId,
                                  BindingResult bindingResult,
                                  Model model,
                                  RedirectAttributes redirectAttributes) {
@@ -73,24 +84,24 @@ public class EmployeeController {
             return "redirect:/employees";
         }
 
-        EmployeeDTO employee = employeeService.createEmployee(createEmployeeRequest);
+        EmployeeDTO employee = employeeService.createEmployee(userId, createEmployeeRequest);
 
         // jika sukses save employee, maka kita tampilkan halaman data_employee
         // kita ambil object employee, lalu cari employee by id
 
-        redirectAttributes.addAttribute("employeeId", employee.getId());
+        model.addAttribute("employeeId", employee.getId());
 
         return "redirect:/employees/{employeeId}"; // redirect ke getEmployeeById
     }
 
-    @GetMapping("/list-employees")
+    @GetMapping("/employees/list-employees")
     public String listEmployees(Model model) {
         List<EmployeeDTO> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("listEmployees", allEmployees);
         return "employee/list_employee";
     }
 
-    @GetMapping("/{employeeId}")
+    @GetMapping("/employees/{employeeId}")
     public String getEmployeeById(@PathVariable(value = "employeeId") String id,
                                   Model model,
                                   RedirectAttributes redirectAttributes) {
@@ -102,7 +113,7 @@ public class EmployeeController {
         return "employee/data_employee";
     }
 
-    @GetMapping("/updateEmployee/{id}")
+    @GetMapping("/employees/updateEmployee/{id}")
     public String showFormForUpdate(@PathVariable(value = "id") String id,
                                     Model model) {
 
@@ -117,14 +128,13 @@ public class EmployeeController {
         return "";
     }
 
-    @GetMapping("/deleteEmployee/{id}")
+    @GetMapping("/employees/deleteEmployee/{id}")
     public String deleteEmployee(@PathVariable(value = "id") String id) {
 
         // call delete employee method
 //        employeeService.deleteEmployeeById(id);
         return "";
     }
-
 
 
 }
