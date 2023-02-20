5576a08b7fb00d3af0be772f6adea197e2d394c7
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-20 18:47:53 +0700

    rename EducationalBackgroundDTO

--- src/main/java/com/bayu/employee/payload/educational/EducationalBackgroundDTO.java
+++ src/main/java/com/bayu/employee/payload/educational/EducationalBackgroundDTO.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EducationBackgroundDTO {
+public class EducationalBackgroundDTO {
 
     private String id;
 
