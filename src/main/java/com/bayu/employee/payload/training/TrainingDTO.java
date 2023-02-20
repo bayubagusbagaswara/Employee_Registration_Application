83dc13c9f80913f01af779a6ffd3fac21f1abd60
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-20 19:38:43 +0700

    add payload TrainingDTO

---
+++ src/main/java/com/bayu/employee/payload/training/TrainingDTO.java
@@ -0,0 +1,23 @@
+package com.bayu.employee.payload.training;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainingDTO {
+
+    private String id;
+
+    private String userId;
+
+    private String trainingName;
+
+    private String certificate;
+
+    private String year;
+}
