76f652d7e22233382625c71be18e48a9fd7febca
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-20 08:44:06 +0700

    add service impl EducationalBackgroundServiceImpl

---
+++ src/main/java/com/bayu/employee/service/EducationalBackgroundServiceImpl.java
@@ -0,0 +1,70 @@
+package com.bayu.employee.service;
+
+import com.bayu.employee.model.EducationalBackground;
+import com.bayu.employee.model.Employee;
+import com.bayu.employee.payload.educational.CreateEducationalBackgroundRequest;
+import com.bayu.employee.payload.educational.EducationBackgroundDTO;
+import com.bayu.employee.repository.EducationalBackgroundRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EducationalBackgroundServiceImpl implements EducationalBackgroundService {
+
+    private final EducationalBackgroundRepository educationalBackgroundRepository;
+    private final UserService userService;
+    private final EmployeeService employeeService;
+
+    public EducationalBackgroundServiceImpl(EducationalBackgroundRepository educationalBackgroundRepository, UserService userService, EmployeeService employeeService) {
+        this.educationalBackgroundRepository = educationalBackgroundRepository;
+        this.userService = userService;
+        this.employeeService = employeeService;
+    }
+
+    @Override
+    public EducationBackgroundDTO createEducationalBackground(String employeeId, CreateEducationalBackgroundRequest createEducationalBackgroundRequest) {
+        // cari employee
+        Employee employee = employeeService.findById(employeeId);
+
+        // create Educational Background
+        EducationalBackground educationalBackground = EducationalBackground.builder()
+                .name(createEducationalBackgroundRequest.getName())
+                .major(createEducationalBackgroundRequest.getMajor())
+                .year(Integer.valueOf(createEducationalBackgroundRequest.getYear()))
+                .build();
+
+        educationalBackgroundRepository.save(educationalBackground);
+
+        return EducationBackgroundDTO.builder()
+                .id(educationalBackground.getId())
+                .name(educationalBackground.getName())
+                .major(educationalBackground.getMajor())
+                .year(String.valueOf(educationalBackground.getYear()))
+                .build();
+    }
+
+    @Override
+    public List<EducationalBackground> findByEmployeeId(String employeeId) {
+//        return educationalBackgroundRepository.findByEmployeeId(employeeId);
+        return null;
+    }
+
+    @Override
+    public List<EducationBackgroundDTO> getByEmployeeId(String employeeId) {
+//        List<EducationalBackground> educationalBackgroundList = educationalBackgroundRepository.findByEmployeeId(employeeId);
+//        return educationalBackgroundList.stream()
+//                .map(educationalBackground -> {
+//                    return EducationBackgroundDTO.builder()
+//                            .id(educationalBackground.getId())
+//                            .employeeId(educationalBackground.getEmployee().getId())
+//                            .name(educationalBackground.getName())
+//                            .major(educationalBackground.getMajor())
+//                            .year(String.valueOf(educationalBackground.getYear()))
+//                            .build();
+//                }).collect(Collectors.toList());
+        return null;
+    }
+
+}
