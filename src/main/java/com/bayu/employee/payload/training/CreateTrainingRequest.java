d552c9f0e5b8362099188c0ef0a8fe6cd6754970
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-20 19:38:05 +0700

    add payload CreateTrainingRequest

---
+++ src/main/java/com/bayu/employee/payload/training/CreateTrainingRequest.java
@@ -0,0 +1,20 @@
+package com.bayu.employee.payload.training;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateTrainingRequest {
+
+    private String trainingName;
+
+    private String certificate;
+
+    private String year;
+
+}
