c86b8fcc9918ccf87feec0b5c0c18ec442e2124b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-20 13:58:07 +0700

    add method mapper

--- src/main/java/com/bayu/employee/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/bayu/employee/service/impl/EmployeeServiceImpl.java
@@ -9,6 +9,7 @@ import com.bayu.employee.payload.employee.UpdateEmployeeRequest;
 import com.bayu.employee.repository.EmployeeRepository;
 import com.bayu.employee.service.EmployeeService;
 import com.bayu.employee.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,55 +28,28 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<EmployeeDTO> getAllEmployees() {
-        return employeeRepository.findAll()
-                .stream()
-                .map(employee -> EmployeeDTO.builder()
-                        .id(employee.getId())
-                        .position(employee.getPosition())
-                        .name(employee.getName())
-                        .age(String.valueOf(employee.getAge()))
-                        .address(employee.getAddress())
-                        .build())
-                .collect(Collectors.toList());
+        return mapToEmployeeDTOList(employeeRepository.findAll());
     }
 
     @Override
     public EmployeeDTO getEmployeeById(String id) {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id : " + id));
-        return EmployeeDTO.builder()
-                .id(employee.getId())
-                .position(employee.getPosition())
-                .name(employee.getName())
-                .age(String.valueOf(employee.getAge()))
-                .address(employee.getAddress())
-                .build();
+        return mapToEmployeeDTO(employee);
     }
 
     @Override
     public EmployeeDTO getEmployeeByName(String name) {
         Employee employee = employeeRepository.findByNameContainsIgnoreCase(name)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with name : " + name));
-        return EmployeeDTO.builder()
-                .id(employee.getId())
-                .position(employee.getPosition())
-                .name(employee.getName())
-                .age(String.valueOf(employee.getAge()))
-                .address(employee.getAddress())
-                .build();
+        return mapToEmployeeDTO(employee);
     }
 
     @Override
     public EmployeeDTO getEmployeeByUserId(String userId) {
         Employee employee = employeeRepository.findByUserId(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with user id : " + userId));
-        return EmployeeDTO.builder()
-                .id(employee.getId())
-                .position(employee.getPosition())
-                .name(employee.getName())
-                .age(String.valueOf(employee.getAge()))
-                .address(employee.getAddress())
-                .build();
+        return mapToEmployeeDTO(employee);
     }
 
     @Override
@@ -92,28 +66,19 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public EmployeeDTO createEmployee(String userId, CreateEmployeeRequest createEmployeeRequest) {
-        // kita harus cek user by id, panggil userService
         User user = userService.findById(userId);
 
-        Employee employee = Employee.builder()
-                .name(createEmployeeRequest.getName())
-                .position(createEmployeeRequest.getPosition())
-                .age(Integer.valueOf(createEmployeeRequest.getAge()))
-                .address(createEmployeeRequest.getAddress())
-                .user(user)
-                .build();
+        Employee employee = new Employee();
+        employee.setPosition(createEmployeeRequest.getPosition().toLowerCase());
+        employee.setName(createEmployeeRequest.getName().toLowerCase());
+        employee.setAge(Integer.valueOf(createEmployeeRequest.getAge()));
+        employee.setGender(createEmployeeRequest.getGender().toLowerCase());
+        employee.setAddress(createEmployeeRequest.getAddress());
+        employee.setUser(user);
 
         employeeRepository.save(employee);
 
-        return EmployeeDTO.builder()
-                .id(employee.getId())
-                .username(employee.getUser().getUsername())
-                .email(employee.getUser().getEmail())
-                .position(employee.getPosition())
-                .name(employee.getName())
-                .age(String.valueOf(employee.getAge()))
-                .address(employee.getAddress())
-                .build();
+        return mapToEmployeeDTO(employee);
     }
 
     @Override
@@ -139,13 +104,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         employeeRepository.save(employee);
 
-        return EmployeeDTO.builder()
-                .id(employee.getId())
-                .position(employee.getPosition())
-                .name(employee.getName())
-                .age(String.valueOf(employee.getAge()))
-                .address(employee.getAddress())
-                .build();
+        return mapToEmployeeDTO(employee);
     }
 
     @Override
@@ -154,6 +113,30 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .orElseThrow(() -> new ResourceNotFoundException("Employee not found with id : " + id));
 
         employeeRepository.delete(employee);
+    }
+
+    public EmployeeDTO mapToEmployeeDTO(Employee employee) {
+        return EmployeeDTO.builder()
+                .id(employee.getId())
+                .position(StringUtils.capitalize(employee.getPosition()))
+                .name(StringUtils.capitalize(employee.getName()))
+                .email(employee.getUser().getEmail())
+                .age(String.valueOf(employee.getAge()))
+                .gender(StringUtils.capitalize(employee.getGender()))
+                .address(employee.getAddress())
+                .build();
+    }
+
+    public List<EmployeeDTO> mapToEmployeeDTOList(List<Employee> employeeList) {
+        return employeeList.stream()
+                .map(this::mapToEmployeeDTO)
+                .collect(Collectors.toList());
+    }
+
+    public static String capitalize(String str)
+    {
+        if (str == null || str.length() == 0) return str;
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
     }
 
 }
