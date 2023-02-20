690c43cda9ba9c3183da7120c06f8087c1af3fb3
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-20 08:45:00 +0700

    add method getEmployeeByUserId

--- src/main/java/com/bayu/employee/repository/EmployeeRepository.java
+++ src/main/java/com/bayu/employee/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.bayu.employee.repository;
 
 import com.bayu.employee.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,6 +13,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, String> {
 
     Optional<Employee> findByNameContainsIgnoreCase(String name);
 
-    Optional<Employee> findByUserId(String id);
+    Optional<Employee> findByUserId(String userId);
+
+    @Query(value = "SELECT e FROM Employee e WHERE e.id_user = :userId", nativeQuery = true)
+    Optional<Employee> getEmployeeByUserId(@Param("userId") String userId);
 
 }
