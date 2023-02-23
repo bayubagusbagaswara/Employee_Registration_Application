d9031bfb84d876c06ce81096ce14f2f792906b3e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-23 09:43:44 +0700

    update EducationController

--- src/main/java/com/bayu/employee/controller/EducationController.java
+++ src/main/java/com/bayu/employee/controller/EducationController.java
@@ -3,6 +3,7 @@ package com.bayu.employee.controller;
 import com.bayu.employee.model.Education;
 import com.bayu.employee.model.User;
 import com.bayu.employee.payload.education.CreateEducationRequest;
+import com.bayu.employee.payload.education.EducationDTO;
 import com.bayu.employee.service.EducationService;
 import com.bayu.employee.service.EmployeeService;
 import com.bayu.employee.service.UserService;
@@ -11,10 +12,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -45,12 +50,12 @@ public class EducationController {
             return "redirect:/education/home";
         }
 
-        List<Education> educationList = educationService.findByUserId(user.getId());
+        List<EducationDTO> educationDTOList = educationService.findAllByUserId(user.getId());
 
         String userId = "";
 
-        for (Education education : educationList) {
-            userId = education.getUser().getId();
+        for (EducationDTO educationDTO : educationDTOList) {
+            userId = educationDTO.getUserId();
         }
 
         // jika sudah ada education minimal 1
@@ -60,8 +65,7 @@ public class EducationController {
 
 
     @GetMapping("/education/home")
-    public String educationHome(Model model) {
-        model.addAttribute("homeEducation", "Home Education");
+    public String educationHome() {
         return "education/home_education";
     }
 
@@ -79,12 +83,31 @@ public class EducationController {
         return "education/add_education";
     }
 
+    @PostMapping("/education/saveEducation/{userId}")
+    public String saveEducation(@Valid @ModelAttribute("createEducationRequest") CreateEducationRequest createEducationRequest,
+                                @PathVariable(value = "userId") String userId,
+                                BindingResult bindingResult,
+                                Model model,
+                                RedirectAttributes redirectAttributes) {
+
+        log.info("Create Education: {}", createEducationRequest.toString());
+        if (bindingResult.hasErrors()) {
+            return "redirect:/education/show-form-education";
+        }
+
+        EducationDTO education = educationService.createEducation(userId, createEducationRequest);
+
+        redirectAttributes.addAttribute("userId", userId);
+        // jika sukses save education, maka tampilkan halaman getAllEducation
+        return "redirect:/education/user/{userId}";
+    }
+
     @GetMapping("/education/user/{userId}")
     public String getAllEducationByUserId(@PathVariable(value = "userId") String userId,
                                           Model model,
                                           RedirectAttributes redirectAttributes) {
 
-        List<Education> educationList = educationService.findByUserId(userId);
+        List<EducationDTO> educationList = educationService.findAllByUserId(userId);
 
         model.addAttribute("educationList", educationList);
 
