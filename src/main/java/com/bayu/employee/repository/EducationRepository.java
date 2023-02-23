e31ea7fef3722c5cfff5d516f82cadb2c0beb84b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-23 09:43:55 +0700

    update EducationRepository

--- src/main/java/com/bayu/employee/repository/EducationRepository.java
+++ src/main/java/com/bayu/employee/repository/EducationRepository.java
@@ -1,6 +1,7 @@
 package com.bayu.employee.repository;
 
 import com.bayu.employee.model.Education;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,5 +10,5 @@ public interface EducationRepository extends JpaRepository<Education, String> {
 
 //    List<EducationalBackground> findAllByEmployeeId(String employeeId);
 
-    List<Education> findAllByUserId(String userId);
+    List<Education> findAllByUserId(String userId, Sort sort);
 }
