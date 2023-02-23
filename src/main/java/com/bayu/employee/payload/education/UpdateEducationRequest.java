55b9698cc82f5605e621bb8c775a2d771e0de5d8
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-23 13:54:33 +0700

    remove validation

--- src/main/java/com/bayu/employee/payload/education/UpdateEducationRequest.java
+++ src/main/java/com/bayu/employee/payload/education/UpdateEducationRequest.java
@@ -5,23 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdateEducationRequest {
 
-    @NotBlank(message = "Level of Education must not be blank")
     private String levelOfEducation;
 
-    @NotBlank(message = "Department must not be blank")
     private String department;
 
-    @NotBlank(message = "College Name must not be blank")
     private String collegeName;
 
-    @NotBlank(message = "Graduation Year must not be blank")
     private String graduationYear;
 }
