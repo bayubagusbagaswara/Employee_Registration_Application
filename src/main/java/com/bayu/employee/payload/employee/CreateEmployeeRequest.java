9ae668a99f52e5da4e0c2d4d06967d961907efcd
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-24 08:11:05 +0700

    change type LocalDate

--- src/main/java/com/bayu/employee/payload/employee/CreateEmployeeRequest.java
+++ src/main/java/com/bayu/employee/payload/employee/CreateEmployeeRequest.java
@@ -4,7 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Data
@@ -27,6 +29,7 @@ public class CreateEmployeeRequest {
 
     private String placeOfBirth;
 
-    private Date dateOfBirth;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateOfBirth;
 
 }
