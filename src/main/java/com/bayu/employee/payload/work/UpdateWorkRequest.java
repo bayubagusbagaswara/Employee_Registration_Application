52d5b06d0ca7332045f0838415761ae157659b73
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-25 11:27:08 +0700

    add payload UpdateWorkRequest

---
+++ src/main/java/com/bayu/employee/payload/work/UpdateWorkRequest.java
@@ -0,0 +1,25 @@
+package com.bayu.employee.payload.work;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateWorkRequest {
+
+    private String companyName;
+
+    private String position;
+
+    private BigDecimal salary;
+
+    private String yearOfEmployment;
+
+    private String yearOfResignation;
+}
