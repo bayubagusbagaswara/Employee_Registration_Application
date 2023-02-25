c17db7ad911e946241ad58d076dc07bdcf7a8cb6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-25 11:28:53 +0700

    add method createWork

--- src/main/java/com/bayu/employee/service/WorkExperienceService.java
+++ src/main/java/com/bayu/employee/service/WorkExperienceService.java
@@ -1,4 +1,10 @@
 package com.bayu.employee.service;
 
+import com.bayu.employee.payload.work.CreateWorkRequest;
+import com.bayu.employee.payload.work.WorkDTO;
+
 public interface WorkExperienceService {
+
+    WorkDTO createWork(String userId, CreateWorkRequest createWorkRequest);
+
 }
