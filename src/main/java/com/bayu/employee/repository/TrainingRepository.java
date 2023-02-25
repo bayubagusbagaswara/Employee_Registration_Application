45eea46f4306350bda38ec8f3e47b502f0fe1c86
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-25 11:13:55 +0700

    method findAllByUserId

--- src/main/java/com/bayu/employee/repository/TrainingRepository.java
+++ src/main/java/com/bayu/employee/repository/TrainingRepository.java
@@ -1,7 +1,12 @@
 package com.bayu.employee.repository;
 
 import com.bayu.employee.model.Training;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TrainingRepository extends JpaRepository<Training, String> {
+
+    List<Training> findAllByUserId(String userId, Sort sort);
 }
