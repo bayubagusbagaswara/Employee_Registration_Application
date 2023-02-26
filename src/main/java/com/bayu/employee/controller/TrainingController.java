418233fa194f69e974ad47dafe3c8c8cf3256c26
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-26 12:39:48 +0700

    add handler training and trainingHome

--- src/main/java/com/bayu/employee/controller/TrainingController.java
+++ src/main/java/com/bayu/employee/controller/TrainingController.java
@@ -1,10 +1,18 @@
 package com.bayu.employee.controller;
 
+import com.bayu.employee.model.User;
+import com.bayu.employee.payload.training.TrainingDTO;
 import com.bayu.employee.service.TrainingService;
 import com.bayu.employee.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 public class TrainingController {
@@ -17,5 +25,40 @@ public class TrainingController {
     public TrainingController(TrainingService trainingService, UserService userService) {
         this.trainingService = trainingService;
         this.userService = userService;
+    }
+
+    @GetMapping("/training")
+    public String training(Authentication authentication,
+                           Model model,
+                           RedirectAttributes redirectAttributes) {
+
+        String username = authentication.getName();
+        log.info("Username: {}", username);
+
+        User user = userService.findByUsername(username);
+
+        if (user.getTrainings().size() == 0) {
+            return "redirect:/training/home";
+        }
+
+        List<TrainingDTO> trainingDTOList = trainingService.getAllTrainingsByUserId(user.getId());
+
+        String userId = "";
+
+        for (TrainingDTO trainingDTO : trainingDTOList) {
+            userId = trainingDTO.getUserId();
+        }
+
+        model.addAttribute("username", username);
+        redirectAttributes.addAttribute("userId", userId);
+
+        return "redirect:/training/user/userId";
+    }
+
+    @GetMapping("/training/home")
+    public String trainingHome(Authentication authentication, Model model) {
+        String username = authentication.getName();
+        model.addAttribute("username", username);
+        return "training/home_training";
     }
 }
