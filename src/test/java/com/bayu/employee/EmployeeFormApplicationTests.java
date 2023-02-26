6fb6c3a1745eb4757b369b6f85e046174dd34564
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-26 09:16:05 +0700

    update testing EmployeeFormApplicationTests

--- src/test/java/com/bayu/employee/EmployeeFormApplicationTests.java
+++ src/test/java/com/bayu/employee/EmployeeFormApplicationTests.java
@@ -3,6 +3,8 @@ package com.bayu.employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -23,4 +25,26 @@ class EmployeeFormApplicationTests {
 		System.out.println("Formatted Date in String format: "+formattedDateStr);
 	}
 
+	@Test
+	void currencyBigDecimalToString() {
+//		public static String format(BigDecimal num) {
+//			java.text.DecimalFormat df = new java.text.DecimalFormat("0.00");
+//			return df.format(num);
+//		}
+		BigDecimal num = new BigDecimal(5000000);
+		DecimalFormat df = new DecimalFormat();
+
+		String format = df.format(num);
+
+		System.out.println(format); // 5.000.000
+	}
+
+	@Test
+	void currencyStringToBigDecimal() {
+		String num = "5000000";
+
+		BigDecimal bigDecimal = new BigDecimal(num);
+
+		System.out.println(bigDecimal); // 5000000
+	}
 }
