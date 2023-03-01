7de025986ee3ad1db09dd836dcb2f9f82bee7746
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-01 11:59:32 +0700

    update WorkController

--- src/main/java/com/bayu/employee/controller/WorkController.java
+++ src/main/java/com/bayu/employee/controller/WorkController.java
@@ -1,8 +1,17 @@
 package com.bayu.employee.controller;
 
+import com.bayu.employee.service.UserService;
+import com.bayu.employee.service.WorkExperienceService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WorkController {
 
+    private final UserService userService;
+    private final WorkExperienceService workExperienceService;
+
+    public WorkController(UserService userService, WorkExperienceService workExperienceService) {
+        this.userService = userService;
+        this.workExperienceService = workExperienceService;
+    }
 }
