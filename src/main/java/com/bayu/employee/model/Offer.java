dbecbbf2559f8e59acdfc2fec8499a16159c7b6b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-01 10:52:50 +0700

    add relation model User

--- src/main/java/com/bayu/employee/model/Offer.java
+++ src/main/java/com/bayu/employee/model/Offer.java
@@ -22,4 +22,14 @@ public class Offer {
 
     @Column(name = "expectation_salary")
     private BigDecimal expectationSalary;
+
+    // bersedia ditempatkan dimana saja, Ya atau Tidak
+    @Column(name = "agreed_placement")
+    private String agreedPlacement;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "id_user", foreignKey = @ForeignKey(name = "fk_offer_id_user"), referencedColumnName = "id")
+    private User user;
+
 }
