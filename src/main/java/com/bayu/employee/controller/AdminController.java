a5a5ecc910fc6b7c7e9329110edcfc1799817606
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-01 13:25:54 +0700

    add handler getEmployeeById, getAllEmployees, deleteEmployee

--- src/main/java/com/bayu/employee/controller/AdminController.java
+++ src/main/java/com/bayu/employee/controller/AdminController.java
@@ -1,8 +1,12 @@
 package com.bayu.employee.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin")
@@ -13,10 +17,33 @@ public class AdminController {
         return "admin/admin.html";
     }
 
-    // bisa get employee by id
+    @GetMapping("/employees")
+    public String getAllEmployees(Authentication authentication, Model model, RedirectAttributes redirectAttributes) {
 
-    // bisa get all employee
+        // tampilkan table all employees
+
+        return "";
+    }
+
+    // bisa get employee by id
+    @GetMapping("/employees/{employeeId}")
+    public String getEmployeeById(@PathVariable(value = "employeeId") String employeeId,
+                                  Authentication authentication,
+                                  Model model,
+                                  RedirectAttributes redirectAttributes) {
+
+        // hanya menampilkan data employee by id
+        return "";
+    }
 
     // bisa delete employee
+    @GetMapping("/employees/delete/{employeeId}")
+    public String deleteEmployee(@PathVariable(value = "employeeId") String employeeId,
+                                 Model model,
+                                 RedirectAttributes redirectAttributes) {
+
+        // delete employee by id
+        return "";
+    }
 
 }
