168c0255450b94b9835722e354be4aac048fe976
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-01 14:37:33 +0700

    add method contract getEmployeeById and getUserById

--- src/main/java/com/bayu/employee/service/AdminService.java
+++ src/main/java/com/bayu/employee/service/AdminService.java
@@ -1,10 +1,19 @@
 package com.bayu.employee.service;
 
+import com.bayu.employee.payload.UserDTO;
+import com.bayu.employee.payload.admin.EmployeeAdminDTO;
+import com.bayu.employee.payload.employee.EmployeeDTO;
+
+import java.util.List;
+
 public interface AdminService {
 
     // get all employees
+    List<EmployeeAdminDTO> getAllEmployees();
 
     // get employee by id
+    EmployeeDTO getEmployeeById(String employeeId);
 
     // get user by id
+    UserDTO getUserById(String userId);
 }
