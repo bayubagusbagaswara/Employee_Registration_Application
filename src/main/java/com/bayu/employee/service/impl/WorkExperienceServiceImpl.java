7c8dccb7dc98a7000b6736ab6d331875f8cd06c1
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-02 16:52:24 +0700

    implement all method

--- src/main/java/com/bayu/employee/service/impl/WorkExperienceServiceImpl.java
+++ src/main/java/com/bayu/employee/service/impl/WorkExperienceServiceImpl.java
@@ -1,5 +1,8 @@
 package com.bayu.employee.service.impl;
 
+import com.bayu.employee.exception.ResourceNotFoundException;
+import com.bayu.employee.model.Employee;
+import com.bayu.employee.model.WorkExperience;
 import com.bayu.employee.payload.work.CreateWorkRequest;
 import com.bayu.employee.payload.work.WorkDTO;
 import com.bayu.employee.repository.WorkExperienceRepository;
@@ -8,6 +11,7 @@ import com.bayu.employee.service.WorkExperienceService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class WorkExperienceServiceImpl implements WorkExperienceService {
@@ -22,16 +26,53 @@ public class WorkExperienceServiceImpl implements WorkExperienceService {
 
     @Override
     public WorkDTO createWorkExperience(String employeeId, CreateWorkRequest createWorkRequest) {
-        return null;
+        // cari employee by id dulu
+        Employee employee = employeeService.findById(employeeId);
+
+        // buat object WorkExperience
+        WorkExperience workExperience = WorkExperience.builder()
+                .position(createWorkRequest.getPosition())
+                .companyName(createWorkRequest.getCompanyName())
+                // salary convert dulu ke BigDecimal
+//                .salary(createWorkRequest.)
+                .lengthOfWork(Float.valueOf(createWorkRequest.getLengthOfWork()))
+                .employee(employee)
+                .build();
+
+        // save work experience
+        workExperienceRepository.save(workExperience);
+
+        return mapToWorkDTO(workExperience);
     }
 
     @Override
     public List<WorkDTO> getAllWorkExperiencesByEmployeeId(String employeeId) {
-        return null;
+        List<WorkExperience> workExperienceList = workExperienceRepository.findAllByEmployeeId(employeeId);
+        return mapToWorkDTOList(workExperienceList);
     }
 
     @Override
     public WorkDTO getWorkExperienceById(String workId) {
-        return null;
+        WorkExperience workExperience = workExperienceRepository.findById(workId).orElseThrow(() -> new ResourceNotFoundException("Work Experience not found with id : " + workId));
+        return mapToWorkDTO(workExperience);
     }
+
+    private WorkDTO mapToWorkDTO(WorkExperience workExperience) {
+        return WorkDTO.builder()
+                .id(workExperience.getId())
+                .employeeId(workExperience.getEmployee().getId())
+                .position(workExperience.getPosition())
+                .companyName(workExperience.getCompanyName())
+                .lengthOfWork(String.valueOf(workExperience.getLengthOfWork()))
+                .salary(String.valueOf(workExperience.getSalary()))
+                .build();
+    }
+
+    private List<WorkDTO> mapToWorkDTOList(List<WorkExperience> workExperienceList) {
+        return workExperienceList.stream()
+                .map(this::mapToWorkDTO)
+                .collect(Collectors.toList());
+    }
+
+    // private convert from String to BigDecimal
 }
