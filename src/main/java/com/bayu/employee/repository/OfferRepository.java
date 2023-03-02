c65f6c2c699169abbcfa85492a7dc778e27c2305
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-02 10:38:39 +0700

    rename method findAllByEmployeeId

--- src/main/java/com/bayu/employee/repository/OfferRepository.java
+++ src/main/java/com/bayu/employee/repository/OfferRepository.java
@@ -2,6 +2,8 @@ package com.bayu.employee.repository;
 
 import com.bayu.employee.model.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OfferRepository extends JpaRepository<Offer, String> {
 }
