f212d73fc4160fac7af5638f37cd3bb562def603
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-02 10:38:36 +0700

    rename method findAllByEmployeeId

--- src/main/java/com/bayu/employee/repository/EducationRepository.java
+++ src/main/java/com/bayu/employee/repository/EducationRepository.java
@@ -3,12 +3,12 @@ package com.bayu.employee.repository;
 import com.bayu.employee.model.Education;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EducationRepository extends JpaRepository<Education, String> {
 
-//    List<EducationalBackground> findAllByEmployeeId(String employeeId);
-
-    List<Education> findAllByUserId(String userId, Sort sort);
+    List<Education> findAllByEmployeeId(String employeeId, Sort sort);
 }
