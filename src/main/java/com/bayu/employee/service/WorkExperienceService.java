7c46398676c52374ec76eb6195b4e92dc66ba42b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-03 09:26:14 +0700

    update WorkExperienceService

--- src/main/java/com/bayu/employee/service/WorkExperienceService.java
+++ src/main/java/com/bayu/employee/service/WorkExperienceService.java
@@ -1,16 +1,16 @@
 package com.bayu.employee.service;
 
 import com.bayu.employee.payload.work.CreateWorkRequest;
-import com.bayu.employee.payload.work.WorkDTO;
+import com.bayu.employee.payload.work.WorkExperienceDTO;
 
 import java.util.List;
 
 public interface WorkExperienceService {
 
-    WorkDTO createWorkExperience(String employeeId, CreateWorkRequest createWorkRequest);
+    WorkExperienceDTO createWorkExperience(String employeeId, CreateWorkRequest createWorkRequest);
 
-    List<WorkDTO> getAllWorkExperiencesByEmployeeId(String employeeId);
+    List<WorkExperienceDTO> getAllWorkExperiencesByEmployeeId(String employeeId);
 
-    WorkDTO getWorkExperienceById(String workId);
+    WorkExperienceDTO getWorkExperienceById(String workId);
 
 }
