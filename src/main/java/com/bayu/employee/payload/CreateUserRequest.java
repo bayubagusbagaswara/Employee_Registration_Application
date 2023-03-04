7fd7c052614854dff1c3f11d50fa77e3a46e81a6
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:45:24 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/CreateUserRequest.java
+++ src/main/java/com/bayu/employee/payload/CreateUserRequest.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateUserRequest {
+public class CreateUserRequest implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = -4327595329533386546L;
 
     private String username;
 
