149914d4f52b64b506baebfb45fe6fadd129b8b9
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:50:52 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/work/CreateWorkRequest.java
+++ src/main/java/com/bayu/employee/payload/work/CreateWorkRequest.java
@@ -5,13 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateWorkRequest {
+public class CreateWorkRequest implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = 4095520802509285186L;
 
     private String companyName;
 
