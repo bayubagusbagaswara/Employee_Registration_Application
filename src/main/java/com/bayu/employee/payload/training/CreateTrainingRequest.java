c0e8ab7e8a2069d23e94eef99f7dfc2a41f13594
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:50:48 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/training/CreateTrainingRequest.java
+++ src/main/java/com/bayu/employee/payload/training/CreateTrainingRequest.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CreateTrainingRequest {
+public class CreateTrainingRequest implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = -489891877436701937L;
 
     private String trainingName;
 
