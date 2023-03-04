7cce11257476a0e48e363f38104bff9fd9a306da
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:51:01 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/training/TrainingDTO.java
+++ src/main/java/com/bayu/employee/payload/training/TrainingDTO.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class TrainingDTO {
+public class TrainingDTO implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = 7931320846186695196L;
 
     private String id;
 
