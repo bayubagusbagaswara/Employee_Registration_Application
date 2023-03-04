75d173145dbcd4b8400df3f13f4f643d91a818fd
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:45:17 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/AuthenticationResponse.java
+++ src/main/java/com/bayu/employee/payload/AuthenticationResponse.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationResponse {
+public class AuthenticationResponse implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = 7894749800380801427L;
 
     private String username;
 }
