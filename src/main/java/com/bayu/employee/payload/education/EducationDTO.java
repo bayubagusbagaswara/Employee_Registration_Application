a256ae49b1b4dab1c90ada23cf64eb2bc6b59843
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:50:54 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/education/EducationDTO.java
+++ src/main/java/com/bayu/employee/payload/education/EducationDTO.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EducationDTO {
+public class EducationDTO implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = 5429895131311271533L;
 
     private String id;
 
