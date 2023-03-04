088622383804551699519d908d3b311e07b7aa59
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:50:56 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/admin/EmployeeAdminDTO.java
+++ src/main/java/com/bayu/employee/payload/admin/EmployeeAdminDTO.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class EmployeeAdminDTO {
+public class EmployeeAdminDTO implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = -1007974275382158669L;
 
     private String position;
     private String nik;
