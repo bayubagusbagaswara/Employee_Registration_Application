e4ff64ce3dca1775614b89c9ecda7bde03e6465e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-03-04 13:45:26 +0700

    add serializable

--- src/main/java/com/bayu/employee/payload/LoginRequest.java
+++ src/main/java/com/bayu/employee/payload/LoginRequest.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class LoginRequest {
+public class LoginRequest implements Serializable {
+
+    @Serial
+    private final static long serialVersionUID = 3532682402554136843L;
 
     private String username;
 
