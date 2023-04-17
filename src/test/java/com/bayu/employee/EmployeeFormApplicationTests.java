dbe9bcba3fa0560abbb7017f3abc819174d44e9d
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-17 08:48:52 +0700

    update testing

--- src/test/java/com/bayu/employee/EmployeeFormApplicationTests.java
+++ src/test/java/com/bayu/employee/EmployeeFormApplicationTests.java
@@ -81,4 +81,36 @@ class EmployeeFormApplicationTests {
 
 		System.out.println(total);
 	}
+
+	public static String formattedTransactionDescription(String soul, String totalMoney) {
+		Integer soulInt = Integer.parseInt(soul);
+		StringBuilder resultSoul = new StringBuilder();
+
+		StringBuilder resultTotalMoney = new StringBuilder();
+		StringBuilder resultString = new StringBuilder();
+
+		if (soulInt > 0) {
+			resultSoul.append(soulInt).append("Jiwa"); // 10Jiwa
+		} else {
+			resultSoul.append("0").append("Jiwa"); // 0Jiwa
+		}
+
+		if (totalMoney.length() <= 6) {
+			resultTotalMoney.append("Rp").append(totalMoney, 0, totalMoney.length() - 3).append("Ribu");
+		}
+
+		if (totalMoney.length() > 6 ) {
+			resultTotalMoney.append("Rp").append(totalMoney, 0, totalMoney.length() - 6).append("Juta");
+		}
+
+		// 10JiwaRp450Ribu-ZakatFitrah
+		return String.valueOf(resultString.append(resultSoul).append(resultTotalMoney).append("-").append("ZakatFitrah"));
+	}
+
+	@Test
+	void test() {
+		String soul = "10";
+		String money = "9000";
+		System.out.println(formattedTransactionDescription(soul, money));
+	}
 }
