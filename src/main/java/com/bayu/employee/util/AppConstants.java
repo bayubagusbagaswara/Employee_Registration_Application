286c218292080aba6f2852a924a9cc01b7eb2508
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-22 09:58:56 +0700

    add util AppConstants

---
+++ src/main/java/com/bayu/employee/util/AppConstants.java
@@ -0,0 +1,18 @@
+package com.bayu.employee.util;
+
+public class AppConstants {
+
+    public final static String CREATE_WORK_EXPERIENCE_REQUEST = "createWorkExperienceRequest";
+    public final static String UPDATE_WORK_EXPERIENCE_REQUEST = "updateWorkExperienceRequest";
+    public final static String FIELD_WORK_EXPERIENCE_POSITION = "position";
+    public final static String FIELD_WORK_EXPERIENCE_COMPANY_NAME = "companyName";
+    public final static String FIELD_WORK_EXPERIENCE_SALARY = "salary";
+    public final static String FIELD_WORK_EXPERIENCE_YEAR_OF_EMPLOYMENT = "yearOfEmployment";
+    public final static String FIELD_WORK_EXPERIENCE_YEAR_OF_RESIGNATION = "yearOfResignation";
+
+    public final static String MESSAGE_VALIDATION_FIELD_WORK_EXPERIENCE_POSITION = "Posisi wajib diisi.";
+    public final static String MESSAGE_VALIDATION_FIELD_WORK_EXPERIENCE_COMPANY_NAME = "Nama Perusahaan wajib diisi.";
+    public final static String MESSAGE_VALIDATION_FIELD_WORK_EXPERIENCE_SALARY = "Gaji wajib diisi.";
+    public final static String MESSAGE_VALIDATION_FIELD_WORK_EXPERIENCE_YEAR_OF_EMPLOYMENT = "Tahun Masuk wajib diisi.";
+    public final static String MESSAGE_VALIDATION_FIELD_WORK_EXPERIENCE_YEAR_OF_RESIGNATION = "Tahun Keluar wajid diisi.";
+}
