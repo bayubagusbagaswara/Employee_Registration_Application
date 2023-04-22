1a846b5201cd131c356a7bb0dbe734436f35d01a
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-22 11:45:29 +0700

    update extend UserAudit

--- src/main/java/com/bayu/employee/model/Role.java
+++ src/main/java/com/bayu/employee/model/Role.java
@@ -1,11 +1,22 @@
 package com.bayu.employee.model;
 
+import com.bayu.employee.model.base.UserAudit;
 import com.bayu.employee.model.enumerator.RoleName;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
-import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "role", uniqueConstraints = @UniqueConstraint(name = "role_name_unique", columnNames = "name"))
@@ -14,9 +25,7 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Role implements Serializable {
-
-    private final static long serialVersionUID = -3116405158206093317L;
+public class Role extends UserAudit {
 
     @Id
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
