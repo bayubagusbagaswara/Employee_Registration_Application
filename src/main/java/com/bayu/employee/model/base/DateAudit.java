389ea9e4733c08c5f0e7902d71e47efc67d375c9
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-22 11:35:55 +0700

    add base model DateAudit

---
+++ src/main/java/com/bayu/employee/model/base/DateAudit.java
@@ -0,0 +1,35 @@
+package com.bayu.employee.model.base;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.Instant;
+
+@MappedSuperclass
+@Data
+//@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = { "createdAt", "updatedAt", "isDeleted" },
+        allowGetters = true
+)
+public abstract class DateAudit implements Serializable {
+
+    private static final long serialVersionUID = 3275762390211340879L;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = Boolean.FALSE;
+
+}
