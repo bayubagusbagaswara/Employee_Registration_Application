01afa119907b42afb65f7244b5b49fe6ca7ebfcd
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-22 15:46:58 +0700

    update method getRoleByName and getAllRoles

--- src/main/java/com/bayu/employee/repository/RoleRepository.java
+++ src/main/java/com/bayu/employee/repository/RoleRepository.java
@@ -6,11 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    @Query("FROM Role r WHERE LOWER(r.name) = LOWER(:name)")
-    Optional<Role> getByName(@RequestParam(name = "name") String name);
+    @Query(value = "SELECT * FROM Role r WHERE LOWER(r.name) = LOWER(:name) AND r.is_deleted = false", nativeQuery = true)
+    Optional<Role> getRoleByName(@RequestParam(name = "name") String name);
+
+    @Query(value = "SELECT * FROM Role r WHERE r.is_deleted = false", nativeQuery = true)
+    List<Role> getAllRoles();
+
 }
