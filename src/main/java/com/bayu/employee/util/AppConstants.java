c1fda84ab82121c965d1c83b94babfc6d73ba011
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-22 18:45:46 +0700

    add several field constant Educational Background

--- src/main/java/com/bayu/employee/util/AppConstants.java
+++ src/main/java/com/bayu/employee/util/AppConstants.java
@@ -1,7 +1,22 @@
 package com.bayu.employee.util;
 
+import org.springframework.validation.FieldError;
+
 public class AppConstants {
 
+    // EDUCATIONAL BACKGROUND
+    public final static String CREATE_EDUCATION_REQUEST = "createEducationRequest";
+    public final static String UPDATE_EDUCATION_REQUEST = "updateEducationRequest";
+    public final static String FIELD_EDUCATION_LEVEL_OF_EDUCATION = "levelOfEducation";
+    public final static String FIELD_EDUCATION_DEPARTMENT = "department";
+    public final static String FIELD_EDUCATION_COLLEGE_NAME = "collegeName";
+    public final static String FIELD_EDUCATION_GRADUATION_YEAR = "graduationYear";
+    public final static String MESSAGE_VALIDATION_FIELD_EDUCATION_LEVEL_OF_EDUCATION = "Tingkat Pendidikan wajib diisi.";
+    public final static String MESSAGE_VALIDATION_FIELD_EDUCATION_DEPARTMENT = "Jurusan wajib diisi.";
+    public final static String MESSAGE_VALIDATION_FIELD_EDUCATION_COLLEGE_NAME = "Nama Instansi/Perguruan Tinggi wajib diisi.";
+    public final static String MESSAGE_VALIDATION_FIELD_EDUCATION_GRADUATION_YEAR = "Tahun Lulus wajib diisi.";
+
+    // WORK EXPERIENCE
     public final static String CREATE_WORK_EXPERIENCE_REQUEST = "createWorkExperienceRequest";
     public final static String UPDATE_WORK_EXPERIENCE_REQUEST = "updateWorkExperienceRequest";
     public final static String FIELD_WORK_EXPERIENCE_POSITION = "position";
