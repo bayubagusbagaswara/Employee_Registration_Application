d60a90ef1d066cc4c487a131f674bf11e5481dbf
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-22 11:36:02 +0700

    add base model UserAudit

---
+++ src/main/java/com/bayu/employee/model/base/UserAudit.java
@@ -0,0 +1,29 @@
+package com.bayu.employee.model.base;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@EqualsAndHashCode(callSuper = true)
+@MappedSuperclass
+@Data
+@JsonIgnoreProperties(
+        value = { "createdBy", "updatedBy" },
+        allowGetters = true
+)
+public abstract class UserAudit extends DateAudit {
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+}
