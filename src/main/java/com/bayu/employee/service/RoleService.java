1a189c2f33341f235825fbdfc2360b65a8a644cc
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-04-22 15:46:26 +0700

    add method getRoleByName, getAllRoles, and deleteRole

--- src/main/java/com/bayu/employee/service/RoleService.java
+++ src/main/java/com/bayu/employee/service/RoleService.java
@@ -3,7 +3,15 @@ package com.bayu.employee.service;
 import com.bayu.employee.payload.CreateRoleRequest;
 import com.bayu.employee.payload.RoleDTO;
 
+import java.util.List;
+
 public interface RoleService {
 
     RoleDTO createRole(CreateRoleRequest createRoleRequest);
+
+    RoleDTO getRoleByName(String name);
+
+    List<RoleDTO> getAllRoles();
+
+    void deleteRole(String roleId);
 }
