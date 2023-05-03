de51f9661c17b30f47b39ffd230b91aebd450df5
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-05-03 10:12:11 +0700

    rename table to Roles

--- src/main/java/com/bayu/employee/repository/RoleRepository.java
+++ src/main/java/com/bayu/employee/repository/RoleRepository.java
@@ -12,10 +12,10 @@ import java.util.Optional;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    @Query(value = "SELECT * FROM Role r WHERE LOWER(r.name) = LOWER(:name) AND r.is_deleted = false", nativeQuery = true)
+    @Query(value = "SELECT * FROM Roles r WHERE LOWER(r.name) = LOWER(:name) AND r.is_deleted = false", nativeQuery = true)
     Optional<Role> getRoleByName(@RequestParam(name = "name") String name);
 
-    @Query(value = "SELECT * FROM Role r WHERE r.is_deleted = false", nativeQuery = true)
+    @Query(value = "SELECT * FROM Roles r WHERE r.is_deleted = false", nativeQuery = true)
     List<Role> getAllRoles();
 
 }
