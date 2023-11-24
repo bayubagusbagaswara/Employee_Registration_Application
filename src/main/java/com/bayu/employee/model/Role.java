3e32d6912f2cb915f64540fab047d68a151df02c
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-11-24 13:06:21 +0700

    update entity Role

--- src/main/java/com/bayu/employee/model/Role.java
+++ src/main/java/com/bayu/employee/model/Role.java
@@ -2,21 +2,12 @@ package com.bayu.employee.model;
 
 import com.bayu.employee.model.base.UserAudit;
 import com.bayu.employee.model.enumerator.RoleName;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "roles", uniqueConstraints = @UniqueConstraint(name = "role_name_unique", columnNames = "name"))
